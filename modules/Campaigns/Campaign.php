3ec78fab746e43810bd1d98354d2526415b30a73
Stephen Ostrow <stephen@ostrow.tech>
2017-11-29 21:59:56 +0000

    Add feature to relate a Note with a Campaign

--- modules/Campaigns/Campaign.php
+++ modules/Campaigns/Campaign.php
@@ -77,8 +77,9 @@ class Campaign extends SugarBean
     public $refer_url;
     public $impressions;
 
-    // These are related
-    public $assigned_user_name;
+	// These are related
+	var $assigned_user_name;
+    var $note_id;
 
     // module name definitions and table relations
     public $table_name = "campaigns";
@@ -87,26 +88,26 @@ class Campaign extends SugarBean
     public $module_dir = 'Campaigns';
     public $importable = true;
 
-    // This is used to retrieve related fields from form posts.
-    public $additional_column_fields = array(
-                'assigned_user_name', 'assigned_user_id',
-    );
+  	// This is used to retrieve related fields from form posts.
+	var $additional_column_fields = array(
+				'assigned_user_name', 'assigned_user_id', 'note_id',
+	);
 
-    public $relationship_fields = array('prospect_list_id'=>'prospect_lists');
+	var $relationship_fields = Array('prospect_list_id'=>'prospect_lists', 'note_id'=>'notes',);
 
-    public $new_schema = true;
-        
-    /**
-     *
-     * @var string
-     */
-    public $survey_id;
-        
-    /**
-     *
-     * @var string
-     */
-    public $survey_name;
+	var $new_schema = true;
+
+        /**
+         *
+         * @var string
+         */
+        public $survey_id;
+
+        /**
+         *
+         * @var string
+         */
+        public $survey_name;
 
     public function list_view_parse_additional_sections(&$listTmpl)
     {
@@ -256,13 +257,14 @@ class Campaign extends SugarBean
         }
 
 
-        // Bug53301
-        if ($this->campaign_type != 'NewsLetter') {
-            $this->frequency = '';
-        }
-        
-        return parent::save($check_notify);
-    }
+		// Bug53301
+		if($this->campaign_type != 'NewsLetter') {
+		    $this->frequency = '';
+		}
+
+		return parent::save($check_notify);
+
+	}
 
 
     public function mark_deleted($id)
