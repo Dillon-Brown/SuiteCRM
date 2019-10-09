1330cf67f6f3baf4dad3631e3202939b78596264
Dillon-Brown <dillon.brown@salesagility.com>
2019-10-09 04:31:50 +0100

    time override

--- tests/unit/phpunit/include/TimeDateTest.php
+++ tests/unit/phpunit/include/TimeDateTest.php
@@ -1,10 +1,23 @@
 <?php
+/**
+ * Overrides time() for testing purposes.
+ * @return int
+ */
+function time()
+{
+    return TimeDateTest::$now ?: \time();
+}
 
 /**
  * Class TimeDateTest
  */
 class TimeDateTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
 {
+    /**
+     * @var int
+     */
+    public static $now;
+
     /**
      * @var TimeDate
      */
@@ -26,5 +39,18 @@ class TimeDateTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
         $result = $this->timeDate->getTimeLapse('2016-01-15 11:16:02');
         $this->assertTrue(isset($result));
         $this->assertInternalType('string', $result);
+    }
+
+    public function testFormatDateString()
+    {
+        $timeArray = [
+            'seconds' => $remainingSeconds,
+            'minutes' => $minutesElapsed,
+            'hours' => $hoursElapsed,
+            'days' => $daysElapsed,
+            'weeks' => $weeksElapsed,
+        ];
+        $timeLapse = $this->timeDate->formatDateString('SugarFeed');
+
     }
 }
