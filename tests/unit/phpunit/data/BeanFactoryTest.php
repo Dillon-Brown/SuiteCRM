8851a1cf13d2f1e7c3cde10744fdaa9929007a94
Dillon-Brown <dillon.brown@salesagility.com>
2019-12-03 16:14:04 +0000

    Fixed PHP Parse error

--- tests/unit/phpunit/data/BeanFactoryTest.php
+++ tests/unit/phpunit/data/BeanFactoryTest.php
@@ -6,13 +6,15 @@ class BeanFactoryTest extends SuitePHPUnitFrameworkTestCase
 {
     public function testUnRegisterEmptyBeanID()
     {
-        $result = (new BeanFactory())::unregisterBean('Accounts', '');
+        $beanFactory = new BeanFactory();
+        $result = $beanFactory::unregisterBean('Accounts', '');
         $this->assertFalse('', $result);
     }
 
     public function testUnRegisterUnloadedBean()
     {
-        $result = (new BeanFactory())::unregisterBean('Accounts', '1');
+        $beanFactory = new BeanFactory();
+        $result = $beanFactory::unregisterBean('Accounts', '1');
         $this->assertFalse('', $result);
     }
 }
