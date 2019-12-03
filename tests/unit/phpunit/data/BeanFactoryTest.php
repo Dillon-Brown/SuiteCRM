e2a60ca246b33daf65617e9abf6982984a2d4dda
Dillon-Brown <dillon.brown@salesagility.com>
2019-12-03 16:02:28 +0000

    Fixed test method naming convention

--- tests/unit/phpunit/data/BeanFactoryTest.php
+++ tests/unit/phpunit/data/BeanFactoryTest.php
@@ -4,13 +4,13 @@ use SuiteCRM\Test\SuitePHPUnitFrameworkTestCase;
 
 class BeanFactoryTest extends SuitePHPUnitFrameworkTestCase
 {
-    public function unRegisterEmptyBeanIDTest()
+    public function testUnRegisterEmptyBeanID()
     {
         $result = (new BeanFactory())::unregisterBean('Accounts', '');
         $this->assertFalse('', $result);
     }
 
-    public function unRegisterUnloadedBeanTest()
+    public function testUnRegisterUnloadedBean()
     {
         $result = (new BeanFactory())::unregisterBean('Accounts', '1');
         $this->assertFalse('', $result);
