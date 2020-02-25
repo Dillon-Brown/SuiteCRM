12b3548137ebf6c92c85564a9ce61ded32ff6899
cameronblaikie <camy.blaikie@gmail.com>
2020-02-25 13:30:12 +0000

    Apiemail1filter Fix - add GetRecondsCest tests

---
+++ tests/api/V8/GetRecordsCest.php
@@ -0,0 +1,55 @@
+<?php
+namespace Test\Api\V8;
+
+use ApiTester;
+use Codeception\Example;
+
+class GetRecordsCest
+{
+    /**
+     * @param ApiTester $I
+     *
+     * @throws \Codeception\Exception\ModuleException
+     */
+    public function _before(ApiTester $I)
+    {
+        $I->login();
+    }
+    /**
+     * @param ApiTester $I
+     * @param Example $example
+     *
+     * @dataProvider shouldWorkDataProvider
+     * @throws \Exception
+     */
+    public function shouldWork(ApiTester $I, Example $example)
+    {
+        /** @var \ArrayIterator $iterator */
+        $iterator = $example->getIterator();
+
+        $I->sendGET($I->getInstanceURL() . $iterator->offsetGet('endPoint'));
+        $I->seeResponseCodeIs(200);
+        $I->seeResponseIsJson();
+        $I->seeResponseContainsJson(['type' => 'fields']);
+        $I->seeResponseContainsJson(
+            [
+                'name' => [
+                    'type' => 'name',
+                    "dbType" => "varchar",
+                ],
+            ]
+        );
+    }
+
+    /**
+     * @return array
+     */
+    protected function shouldWorkDataProvider()
+    {
+        return [
+            [
+                'endPoint' => '/Api/V8/module/Accounts',
+            ],
+        ];
+    }
+}
