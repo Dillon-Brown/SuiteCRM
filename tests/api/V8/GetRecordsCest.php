3c1baa13a7c6b283664b7dd9a649f3c78fe9de4a
Dillon-Brown <dillon.brown@salesagility.com>
2020-03-06 09:19:18 +0000

    Email1 api test
    Signed-off-by: Dillon-Brown <dillon.brown@salesagility.com>

--- tests/api/V8/GetRecordsCest.php
+++ tests/api/V8/GetRecordsCest.php
@@ -2,14 +2,17 @@
 namespace Test\Api\V8;
 
 use ApiTester;
+use ArrayIterator;
 use Codeception\Example;
+use Codeception\Exception\ModuleException;
+use Exception;
 
 class GetRecordsCest
 {
     /**
      * @param ApiTester $I
      *
-     * @throws \Codeception\Exception\ModuleException
+     * @throws ModuleException
      */
     public function _before(ApiTester $I)
     {
@@ -20,125 +23,24 @@ class GetRecordsCest
      * @param Example $example
      *
      * @dataProvider shouldWorkDataProvider
-     * @throws \Exception
+     * @throws Exception
      */
     public function shouldWork(ApiTester $I, Example $example)
     {
-        /** @var \ArrayIterator $iterator */
+        /** @var ArrayIterator $iterator */
         $iterator = $example->getIterator();
 
         $I->sendGET($I->getInstanceURL() . $iterator->offsetGet('endPoint'));
         $I->seeResponseCodeIs(200);
         $I->seeResponseIsJson();
-        $I->seeResponseContainsJson(
-            [
-                'data' => [
-                     'meta' => [
-                        'type' => 'Account',
-                        'id' => '22ad4c3d-dcff-0ec9-5f16-5e569fd3e780',
-                        'attributes' => ['"name" => "Southern Realty",
-                                        "date_entered" => "2020-02-26T16 =>38 =>00+00 =>00",
-                                        "date_modified" => "2020-03-03T09 =>34 =>00+00 =>00",
-                                        "modified_user_id" => "1",
-                                        "modified_by_name" => "Administrator",
-                                        "created_by" => "1",
-                                        "created_by_name" => "Administrator",
-                                        "description" => "",
-                                        "deleted" => "0",
-                                        "created_by_link" => "",
-                                        "modified_user_link" => "",
-                                        "assigned_user_id" => "seed_chris_id",
-                                        "assigned_user_name" => "Chris Olliver",
-                                        "assigned_user_link" => "",
-                                        "SecurityGroups" => "",
-                                        "account_type" => "Customer",
-                                        "industry" => "Insurance",
-                                        "annual_revenue" => "",
-                                        "phone_fax" => "",
-                                        "billing_address_street" => "321 University Ave.",
-                                        "billing_address_street_2" => "",
-                                        "billing_address_street_3" => "",
-                                        "billing_address_street_4" => "",
-                                        "billing_address_city" => "Cupertino",
-                                        "billing_address_state" => "--allNY",
-                                        "billing_address_postalcode" => "15186",
-                                        "billing_address_country" => "USA",
-                                        "rating" => "",
-                                        "phone_office" => "(621) 865-5301",
-                                        "phone_alternate" => "",
-                                        "website" => "www.beansinfo.name",
-                                        "ownership" => "",
-                                        "employees" => "",
-                                        "ticker_symbol" => "",
-                                        "shipping_address_street" => "321 University Ave.",
-                                        "shipping_address_street_2" => "",
-                                        "shipping_address_street_3" => "",
-                                        "shipping_address_street_4" => "",
-                                        "shipping_address_city" => "Cupertino",
-                                        "shipping_address_state" => "NY",
-                                        "shipping_address_postalcode" => "15186",
-                                        "shipping_address_country" => "USA",
-                                        "email1" => "kid.hr.beans@example.com",
-                                        "email_addresses_primary" => "",
-                                        "email_addresses" => "",
-                                        "email_addresses_non_primary" => "",
-                                        "parent_id" => "",
-                                        "sic_code" => "",
-                                        "parent_name" => "",
-                                        "members" => "",
-                                        "member_of" => {},
-                                        "email_opt_out" => "0",
-                                        "invalid_email" => "0",
-                                        "cases" => "",
-                                        "email" => "",
-                                        "tasks" => "",
-                                        "notes" => "",
-                                        "meetings" => "",
-                                        "calls" => "",
-                                        "emails" => "",
-                                        "documents" => "",
-                                        "bugs" => "",
-                                        "contacts" => "",
-                                        "opportunities" => "",
-                                        "project" => "",
-                                        "leads" => "",
-                                        "campaigns" => "",
-                                        "campaign_accounts" => {},
-                                        "campaign_id" => "",
-                                        "campaign_name" => "",
-                                        "prospect_lists" => "",
-                                        "aos_quotes" => "",
-                                        "aos_invoices" => "",
-                                        "aos_contracts" => "",
-                                        "jjwg_maps_lng_c" => "",
-                                        "jjwg_maps_lat_c" => "",
-                                        "jjwg_maps_address_c" => "",
-                                        "jjwg_maps_geocode_status_c" => "",
-                                        '],
-                        'relationships' => ['AOS_Contracts' => [],
-                                            'AOS_Invoices' => [],
-                                            'AOS_Quotes' => [],
-                                            'Accounts' => [],
-                                            'Bugs' => [],
-                                            'Calls' => [],
-                                            'CampaignLog' => [],
-                                            'Cases' => [],
-                                            'Contacts' => [],
-                                            'EmailAddress' => [],
-                                            'Emails' => [],
-                                            'Leads' => [],
-                                            'Meetings' => [],
-                                            'Notes' => [],
-                                            'Opportunities' => [],
-                                            'Project' => [],
-                                            'ProspectLists' => [],
-                                            'SecurityGroups' => [],
-                                            'Tasks' => [],
-                                            'Users' => [],
-                                        ],
-                    ],
-                ],
-            ]
+        $responseJson = $I->grabResponse();
+        $responseArray = json_decode($responseJson, true);
+        $I->assertNotEmpty($responseArray);
+        $I->assertArrayHasKey('email1', $responseArray['data']['attributes']);
+
+        $I->assertEquals(
+            'vegan.vegan.the@example.net',
+            $responseArray['data']['attributes']['email1']
         );
     }
 
@@ -149,7 +51,7 @@ class GetRecordsCest
     {
         return [
             [
-                'endPoint' => '/Api/V8/module/Accounts/?filter[email1][eq]=kid.hr.beans@example.com',
+                'endPoint' => '/Api/V8/module/Accounts/11c34eba-76e0-8f0d-6f1d-5e567b77e52c',
             ],
         ];
     }
